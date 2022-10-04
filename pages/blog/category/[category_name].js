54b47a7aaa20c08693a3ebd01619dfa82599edc5
herman <hermanconnor11@gmail.com>
2022-10-04 00:18:38 -0400

    Refactor static props function

--- pages/blog/category/[category_name].js
+++ pages/blog/category/[category_name].js
@@ -1,11 +1,10 @@
 import fs from 'fs';
 import path from 'path';
-import Link from 'next/link';
 import matter from 'gray-matter';
 
 import Layout from '@/components/Layout';
 import Post from '@/components/Post';
-import { sortByDate } from '@/utils/index';
+import { getPosts } from '@/lib/posts';
 
 export default function CategoryPage({ posts, categoryName }) {
   return (
@@ -50,21 +49,7 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params: { category_name } }) {
   const files = fs.readdirSync(path.join('posts'));
 
-  const posts = files.map((filename) => {
-    const slug = filename.replace('.md', '');
-
-    const markdownWithMeta = fs.readFileSync(
-      path.join('posts', filename),
-      'utf-8',
-    );
-
-    const { data: frontmatter } = matter(markdownWithMeta);
-
-    return {
-      slug,
-      frontmatter,
-    };
-  });
+  const posts = getPosts();
 
   // Filter By Category
   const categoryPosts = posts.filter(
@@ -73,7 +58,7 @@ export async function getStaticProps({ params: { category_name } }) {
 
   return {
     props: {
-      posts: categoryPosts.sort(sortByDate),
+      posts: categoryPosts,
       categoryName: category_name,
     },
   };
