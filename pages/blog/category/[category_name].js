1c9afba6eb56d34668c7afc54449da44d246ffca
herman <hermanconnor11@gmail.com>
2022-10-04 00:50:54 -0400

    Add category sidebar

--- pages/blog/category/[category_name].js
+++ pages/blog/category/[category_name].js
@@ -4,19 +4,28 @@ import matter from 'gray-matter';
 
 import Layout from '@/components/Layout';
 import Post from '@/components/Post';
+import CategoryList from '@/components/CategoryList';
 import { getPosts } from '@/lib/posts';
 
-export default function CategoryPage({ posts, categoryName }) {
+export default function CategoryPage({ posts, categoryName, categories }) {
   return (
     <Layout>
-      <h1 className='text-5xl border-b-4 p-5 font-bold'>
-        Blogs in {categoryName}
-      </h1>
+      <div className='flex justify-between'>
+        <div className='w-3/4 mr-10'>
+          <h1 className='text-5xl border-b-4 p-5 font-bold'>
+            Blogs in {categoryName}
+          </h1>
 
-      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
-        {posts.map((post, index) => (
-          <Post key={index} post={post} />
-        ))}
+          <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            {posts.map((post, index) => (
+              <Post key={index} post={post} />
+            ))}
+          </div>
+        </div>
+
+        <div className='w-1/4'>
+          <CategoryList categories={categories} />
+        </div>
       </div>
     </Layout>
   );
@@ -47,9 +56,11 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params: { category_name } }) {
-  const files = fs.readdirSync(path.join('posts'));
-
   const posts = getPosts();
+
+  // Get categories for sidebar
+  const categories = posts.map((post) => post.frontmatter.category);
+  const uniqueCategories = [...new Set(categories)];
 
   // Filter By Category
   const categoryPosts = posts.filter(
@@ -60,6 +71,7 @@ export async function getStaticProps({ params: { category_name } }) {
     props: {
       posts: categoryPosts,
       categoryName: category_name,
+      categories: uniqueCategories,
     },
   };
 }
