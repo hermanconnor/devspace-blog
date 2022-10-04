8dbfef472f50dbdcac6ea50c4593df8a259560bb
herman <hermanconnor11@gmail.com>
2022-10-04 15:20:02 -0400

    Cache posts

--- pages/api/search.js
+++ pages/api/search.js
@@ -6,7 +6,9 @@ export default function handler(req, res) {
   let posts;
 
   if (process.env.NODE_ENV === 'production') {
-    // FETCH FROM CACHE: TODO
+    // Fetch from cache
+
+    posts = require('../../cache/data').posts;
   } else {
     const files = fs.readdirSync(path.join('posts'));
 
